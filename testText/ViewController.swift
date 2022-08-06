c66bcdf9f98f098840c896bc0b4af22018c9f93a
Hiro-im <doraemon.for.private@gmail.com>
2022-08-06 17:08:55 +0900

    Initial Commit

---
+++ testText/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testText
+//
+//  Created by 今橋浩樹 on 2022/08/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
